16b26d30f4b0de356abee8de16556a7aae56aae3
beomsoo lee <cath7512@naver.com>
2025-02-18 16:47:45 +0545

    WIP: 로컬 변경 사항 커밋

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -1,202 +1,190 @@
 <template>
-    <div id="map"></div>
-  </template>
-  
-  <script>
-  /* global google */
-  import { createApp, h } from 'vue'; // Import Vue and h function
-  import { GOOGLE_MAPS_API_KEY, GOOGLE_MAP_ID } from '../constants/constants';
-  import { createWeatherForecastChart, createWeatherHistoryChart } from './WeatherGraph';
-  import { createEconomyChart } from './EconomyGraph';
-  import TabYoutube from './TabYoutube.vue'; // Correct the import statement
+  <div id="map"></div>
+</template>
 
-  import '../css/GoogleMaps.css';
-  import '../css/myPlanner.css';
-  import cities from '../assets/city.json';
-  
-  export default {
-    name: 'GoogleMaps',
-    components: {
-      TabYoutube
+<script>
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { createApp, h } from 'vue';
+import { GOOGLE_MAPS_API_KEY, GOOGLE_MAP_ID } from '../constants/constants';
+import { createWeatherForecastChart, createWeatherHistoryChart } from './WeatherGraph';
+import { createEconomyChart } from './EconomyGraph';
+import TabYoutube from './TabYoutube.vue';
+import '../css/GoogleMaps.css';
+import '../css/myPlanner.css';
+import cities from '../assets/city.json';
+
+export default {
+  name: 'GoogleMaps',
+  data() {
+    return {
+      openInfoWindow: null,
+      lastActiveTab: 'weather',
+      youTubeSearch: null
+    };
+  },
+  mounted() {
+    this.loadScript();
+  },
+  methods: {
+    loadScript() {
+      if (typeof google === 'object' && typeof google.maps === 'object') {
+        this.initMap();
+        return;
+      }
+
+      if (document.getElementById('google-maps-script')) {
+        return;
+      }
+
+      window.initMap = this.initMap.bind(this);
+
+      const script = document.createElement('script');
+      script.id = 'google-maps-script';
+      script.src = `https://maps.googleapis.com/maps/api/js?loading=async&key=${GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=marker&units=metric`;
+      script.defer = true;
+      document.head.appendChild(script);
     },
-    data() {
-        return {
-        openInfoWindow: null,
-        lastActiveTab: 'weather', // Remember the last active tab
-        youTubeSearch: null
-        };
-    },    
-    mounted() {
-      this.loadScript();
+    async initMap() {
+      const mapElement = document.getElementById('map');
+      if (!mapElement) return;
+
+      const map = new google.maps.Map(mapElement, {
+        center: { lat: 36.2048, lng: 127.1025 },
+        zoom: 3,
+        mapId: GOOGLE_MAP_ID
+      });
+
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+      cities.forEach(city => {
+        this.createMarker(city, map, AdvancedMarkerElement);
+      });
     },
-    methods: {
-      loadScript() {
-        // Check if the script is ALREADY loaded
-        if (typeof google === 'object' && typeof google.maps === 'object') {
-            this.initMap(); // Initialize map if already loaded
-            return;
+    createMarker(city, map, AdvancedMarkerElement) {
+      const div = document.createElement('div');
+      div.innerHTML = `<div class="label">${city.name}</div>`;
+      div.classList.add('custom-marker');
+
+      const marker = new AdvancedMarkerElement({
+        map,
+        position: { lat: city.coordinates.lat, lng: city.coordinates.lng },
+        content: div
+      });
+
+      const infoWindowContent = this.createInfoWindow(city);
+      const infoWindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+      });
+
+      marker.addListener('click', () => {
+        if (this.openInfoWindow) {
+          this.openInfoWindow.close();
         }
-        
-        if (document.getElementById('google-maps-script')) {
-                return; // If script element exists, do nothing.  It's likely still loading.
-        }
-  
-        window.initMap = this.initMap;
-  
-        const script = document.createElement('script');
-        script.id = 'google-maps-script';
-        script.src = `https://maps.googleapis.com/maps/api/js?loading=async&key=${GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=marker&units=metric`;
-        script.defer = true;
-        document.head.appendChild(script);
-      },
-      async initMap() {
-        const mapElement = document.getElementById('map');
-        if (!mapElement) return;
-  
-        const map = new google.maps.Map(mapElement, {
-          center: { lat: 36.2048, lng: 127.1025 },
-          zoom: 3,
-          mapId: GOOGLE_MAP_ID
+        this.youTubeSearch = `${city.country_name} ${city.name}`;
+        infoWindow.open(map, marker);
+        this.openInfoWindow = infoWindow;
+        google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+          createWeatherForecastChart(city);
+          createWeatherHistoryChart(city);
+          createEconomyChart(city);
+          this.setupTabs();
         });
-  
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  
-        cities.forEach(city => {
-          this.createMarker(city, map, AdvancedMarkerElement);
-        });
-      },
-      createMarker(city, map, AdvancedMarkerElement) {
-        const div = document.createElement('div');
-        div.innerHTML = `<div class="label">${city.name}</div>`;
-        div.classList.add('custom-marker');
-  
-        const marker = new AdvancedMarkerElement({
-          map,
-          position: { lat: city.coordinates.lat, lng: city.coordinates.lng },
-          content: div
-        });
-  
-        const infoWindowContent = this.createInfoWindow(city);
-        const infoWindow = new google.maps.InfoWindow({
-          content: infoWindowContent
-        });
-  
-        marker.addListener('click', () => {
-          if (this.openInfoWindow) {
-            this.openInfoWindow.close();
-          }
-          this.youTubeSearch = `${city.country_name} ${city.name}`; // Set the current city when marker is clicked
-          infoWindow.open(map, marker);
-          this.openInfoWindow = infoWindow;
-          google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
-            createWeatherForecastChart(city);
-            createWeatherHistoryChart(city);
-            createEconomyChart(city);
-            this.setupTabs();
-          });
-        });
-      },
-      createInfoWindow(city) {
-        const flagUrl = `/assets/country_flags/${city.code}.png`;
-  
-        return `
-          <div class="info-window">
-            <div class="header">
-              <img src="${flagUrl}" alt="${city.code} flag" class="flag"/>
-              &nbsp;<b>${city.country_name || 'N/A'}</b>&nbsp;&nbsp;${city.name}
+      });
+    },
+    createInfoWindow(city) {
+      const flagUrl = `/assets/country_flags/${city.code}.png`;
+      return `
+        <div class="info-window">
+          <div class="header">
+            <img src="${flagUrl}" alt="${city.code} flag" class="flag"/>
+            &nbsp;<b>${city.country_name || 'N/A'}</b>&nbsp;&nbsp;${city.name}
+          </div>
+          <div class="content">
+            <div class="tabs">
+              <button class="tab-button" data-tab="weather">
+                <img src="../assets/icons/weather.png" alt="날씨 아이콘">
+              </button>
+              <button class="tab-button" data-tab="economy">
+                <img src="../assets/icons/money.png" alt="경제 아이콘">
+              </button>
+              <button class="tab-button" data-tab="events">
+                <img src="../assets/icons/accident.png" alt="사건 아이콘">
+              </button>
+              <button class="tab-button" data-tab="youtube">
+                <img src="../assets/icons/youtube.svg" alt="유튜브 아이콘">
+              </button>
             </div>
-            <div class="content">
-              <div class="tabs">
-                <button class="tab-button" data-tab="weather">
-                  <img src="../assets/icons/weather.png" alt="날씨 아이콘">
-                </button>
-                <button class="tab-button" data-tab="economy">
-                  <img src="../assets/icons/money.png" alt="경제 아이콘">
-                </button>
-                <button class="tab-button" data-tab="events">
-                  <img src="../assets/icons/accident.png" alt="사건 아이콘">
-                </button>
-                <button class="tab-button" data-tab="youtube">
-                  <img src="../assets/icons/youtube.svg" alt="유튜브 아이콘">
-                </button>
+            <div class="tab-contents">
+              <div class="tab-content" id="weather">
+                <h3 class="chart-title">5 Days Forecast</h3>
+                <canvas id="weatherForecastChart" width="400" height="200"></canvas>
+                <br>
+                <h3 class="chart-title">2024 Weather History</h3>
+                <canvas id="weatherHistoryChart" width="400" height="300"></canvas>
               </div>
-              <div class="tab-contents">
-                <div class="tab-content" id="weather">
-                  <h3 class="chart-title">5 Days Forecast</h3>
-                  <canvas id="weatherForecastChart" width="400" height="200"></canvas>
-                  <br>
-                  <h3 class="chart-title">2024 Weather History</h3>
-                  <canvas id="weatherHistoryChart" width="400" height="300"></canvas>
-                </div>
-                <div class="tab-content" id="economy">
-                    <canvas id="economyChart"></canvas>
-                </div>
-                <div class="tab-content" id="events">
-                  <p>사건 정보가 여기에 표시됩니다.</p>
-                </div>
-                <div class="tab-content" id="youtube">
-                  <div id="youtube-container"></div>
-                </div>
+              <div class="tab-content" id="economy">
+                  <canvas id="economyChart"></canvas>
+              </div>
+              <div class="tab-content" id="events">
+                <p>사건 정보가 여기에 표시됩니다.</p>
+              </div>
+              <div class="tab-content" id="youtube">
+                <div id="youtube-container"></div>
               </div>
             </div>
           </div>
-        `;
-      },
-      setupTabs() {
-        const tabButtons = document.querySelectorAll('.tab-button');
-        const tabContents = document.querySelectorAll('.tab-content');
-
-        tabButtons.forEach(button => {
-          button.addEventListener('click', () => {
-            // 1. 모든 탭 버튼과 콘텐츠 비활성화
-            tabButtons.forEach(btn => btn.classList.remove('active'));
-            tabContents.forEach(content => {
-              content.style.display = 'none'; // display: none으로 설정
-              content.classList.remove('active');
-            });
-
-            // 2. 클릭한 탭 버튼과 콘텐츠 활성화
-            button.classList.add('active');
-            const targetContent = document.getElementById(button.dataset.tab);
-            targetContent.style.display = 'block'; // display: block으로 설정
-            targetContent.classList.add('active');
-
-            // Remember the last active tab
-            this.lastActiveTab = button.dataset.tab;
-
-            if (this.lastActiveTab === 'youtube') {
-              this.mountYoutubeComponent();
-            }
+        </div>
+      `;
+    },
+    setupTabs() {
+      const tabButtons = document.querySelectorAll('.tab-button');
+      const tabContents = document.querySelectorAll('.tab-content');
+      tabButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          tabButtons.forEach(btn => btn.classList.remove('active'));
+          tabContents.forEach(content => {
+            content.style.display = 'none';
+            content.classList.remove('active');
           });
-        });
 
-        // 초기 탭 설정
-        const initialTab = document.querySelector(`.tab-button[data-tab="${this.lastActiveTab}"]`) || document.querySelector('.tab-button[data-tab="weather"]');
-        initialTab.classList.add('active'); // Activate the initial tab button
-        
-        const initialContent = document.getElementById(this.lastActiveTab) || document.getElementById('weather');
-        initialContent.style.display = 'block'; // Show the initial tab content
+          button.classList.add('active');
+          const targetContent = document.getElementById(button.dataset.tab);
+          targetContent.style.display = 'block';
+          targetContent.classList.add('active');
 
-        tabContents.forEach(content => {
-          if (content !== initialContent) {
-            content.style.display = 'none';  // Hide other tab contents
+          this.lastActiveTab = button.dataset.tab;
+          if (this.lastActiveTab === 'youtube') {
+            this.mountYoutubeComponent();
           }
         });
+      });
 
-        if (this.lastActiveTab === 'youtube') {
-          this.mountYoutubeComponent();
+      const initialTab = document.querySelector(`.tab-button[data-tab="${this.lastActiveTab}"]`) || document.querySelector('.tab-button[data-tab="weather"]');
+      initialTab.classList.add('active');
+
+      const initialContent = document.getElementById(this.lastActiveTab) || document.getElementById('weather');
+      initialContent.style.display = 'block';
+
+      tabContents.forEach(content => {
+        if (content !== initialContent) {
+          content.style.display = 'none';
         }
-      },
-      mountYoutubeComponent() {
-        const container = document.getElementById('youtube-container');
-        if (container && this.youTubeSearch) {
-          createApp({
-            render: () => h(TabYoutube, {
-              youTubeSearch: this.youTubeSearch
-            })
-          }).mount(container);
-        }
-      },
+      });
+      if (this.lastActiveTab === 'youtube') {
+        this.mountYoutubeComponent();
+      }
+    },
+    mountYoutubeComponent() {
+      const container = document.getElementById('youtube-container');
+      if (container && this.youTubeSearch) {
+        createApp({
+          render: () => h(TabYoutube, {
+            youTubeSearch: this.youTubeSearch
+          })
+        }).mount(container);
+      }
     }
-  };
-  </script>
+  }
+};
+</script>
